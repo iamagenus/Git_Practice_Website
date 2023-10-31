4921f99e87b7e24c598cb163421f9c74228e584c
Iamagenus <brandonlewandowski@icloud.com>
2023-10-31 15:48:25 -0400

    added js

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Function to change the text color
+function changeTextColor() {
+    document.querySelector("p").style.color = "blue";
+}
+
+// Call the function
+changeTextColor();
